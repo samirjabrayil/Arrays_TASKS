4ca2844cb4bee80dadbd04c90a53f9696eddd956
[Samir Jabrayilzada] <[samirej@code.edu.az]>
2023-01-05 11:47:36 +0400

    Add project files.

---
+++ ArrayTask/Program.cs
@@ -0,0 +1,264 @@
+﻿namespace ArrayTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TASK 1 (Menfi elementlerin sayini ve cemini tapan program)
+
+            //Console.Write("Massivin uzunlugunu daxil edin : ");
+            //int length = int.Parse(Console.ReadLine());
+            //int[] array = new int[length];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}-ci elementi daxil edin: ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    if (array[j] < 0)
+            //    {
+            //        sum += array[j];
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"Menfi ededlerin SAYI : {count}");
+
+            //Console.WriteLine($"Menfi ededlerin CEMI : {sum}");
+
+
+            #endregion
+
+            #region TASK 2 (5'e bolunen ve 7'e bolunmeyen elementlerin cemini ve sayini tapan program)
+
+            //Console.Write("Massivin uzunlugunu daxil edin : ");
+            //int length = int.Parse(Console.ReadLine());
+
+            //int[] array = new int[length];
+
+            //int count = 0;
+            //int sum = 0;
+
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}- ci elementi daxil edin: ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    if (array[j] % 5 == 0 && array[j] % 7 != 0)
+            //    {
+            //        sum += array[j];
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"5'e bolunen ve 7'e bolunmeyen elementlerin sayi : {count}");
+
+            //Console.WriteLine($"5 e bolunen ve 7 ye bolunmeyen elementlerin cemi : {sum}");
+
+            #endregion
+
+            #region TASK 3 (Eyni zamanda musbet ve tek olan elementlerin cemini ve sayini tapan program)
+
+            //Console.Write("Massivin uzunlugunu daxil edin : ");
+            //int length = int.Parse(Console.ReadLine());
+            //int[] array = new int[length];
+
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}-ci elementi daxil edin : ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    if (array[j] > 0 && array[j] % 2 != 0)
+            //    {
+            //        sum += array[j];
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"Eyni zamanda MUSBET ve TEK olan elementlerin sayi : {count}");
+
+            //Console.WriteLine($"Eyni zamanda MUSBET ve TEK olan elementlerin cemi : {sum}");
+
+            #endregion
+
+            #region TASK 4 (Cut yerde olan cut elementlerin cemini ve sayini tapan program)
+
+            //Console.Write("Massivin uzunlugunu daxil edin : ");
+            //int length = int.Parse(Console.ReadLine());
+            //int[] array = new int[length];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}-ci elementi daxil edin: ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    if (j % 2 != 0 && array[j] % 2 == 0)
+            //    {
+            //        sum += array[j];
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"Cut yerde olan cut elementlerin sayi : {count}");
+
+            //Console.WriteLine($"Cut yerde olan cut elementlerin cemi : {sum}");
+
+            #endregion
+
+            #region TAKS 5  (Tek yerde olan cut elementlerin cemini ve sayini tapan program)
+
+            //Console.Write("Massivin uzunlugunu daxil edin : ");
+            //int length = int.Parse(Console.ReadLine());
+            //int[] array = new int[length];
+            //int count = 0; 
+            //int sum = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}-ci elementi daxil edin : ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    if (j % 2 == 0 && array[j] % 2 == 0)
+            //    {
+            //        sum += array[j];
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"Tek yerde olan cut elementlerin sayi : {count}");
+
+            //Console.WriteLine($"Tek yerde olan cut elementlerin cemi : {sum}");
+
+            #endregion
+
+            #region TASK 6  (Cut yerde olan tek elementlerin cemini ve sayini tapan program)
+
+
+            //Console.Write("Massivin uzunlugunu daxil edin : ");
+            //int length = int.Parse(Console.ReadLine());
+            //int[] array = new int[length];
+            //int count = 0, sum = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}-ci elementi daxil edin : ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    if (j % 2 != 0 && array[j] % 2 != 0)
+            //    {
+            //        sum += array[j];
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"Cut yerde olan tek elementlerin sayi : {count}");
+
+            //Console.WriteLine($"Cut yerde olan tek elementlerin cemi : {sum}");
+
+            #endregion
+
+            #region TASK 7 (7'e bolen zaman qaligi 1;2;5 olan elementlerin sayini tapan program)
+
+            //Console.Write("Massivin uzunlugunu daxil edin :");
+            //int length = int.Parse(Console.ReadLine());
+            //int[] array = new int[length];
+            //int count = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}-ci elementi daxil edin :");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    if (array[j] % 7 == 1 || array[j] % 7 == 2 || array[j] % 7 == 5)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"7'e bolen zaman qaligi 1;2;5 olan elementlerin sayi : {count}");
+
+            #endregion
+
+            #region TASK 9 (Birinci Maksimal elementin nomresini tapan program)
+
+            //Console.Write("Massivin uzunlugunu daxil edin : ");
+            //int length = int.Parse(Console.ReadLine());            
+            //int[] array = new int[length];
+
+            //int max = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}-ci elementi daxil edin: ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 1; j < array.Length; j++)
+            //{
+
+            //    if (array[j] > array[max])
+            //        max = j;
+            //}
+
+            //Console.WriteLine($"Birinci Maksimal elementin nómresi: {max}");
+
+            #endregion
+
+            #region TASK 10 (Birinci max elementin nomresini tapAN program)
+
+            //Console.Write("Massivin uzunlugunu daxil edin :");
+            //int length = int.Parse(Console.ReadLine());
+            //int[] array = new int[length];
+            //int max = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{i}-ci elementi daxil edin : ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int j = 1; j < array.Length; j++)
+            //{
+
+            //    if (array[j] >= array[max])
+            //        max = j;
+            //}
+
+            //Console.WriteLine($"Sonuncu max elementin nomresi: {max}");
+
+            #endregion
+
+
+
+        }
+    }
+}
